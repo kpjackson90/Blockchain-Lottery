24261645c7f7a43473ecfd08de137bc1f2799ddb
Kareem Phillip-Jackson <kpjackson27@gmail.com>
2018-09-29 14:03:05 -0400

    setup infura to deploy contract 2

--- deploy.js
+++ deploy.js
@@ -7,3 +7,17 @@ const provider = new HDWalletProvider(
   "https://rinkeby.infura.io/v3/71c20fe2b4bb4ef2a67919eff2a9d26f"
 );
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  //contract deployment statement
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hello There"] })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Address contract was deployed to: ", result.options.address);
+};
+deploy();
